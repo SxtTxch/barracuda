a02869a0912c630b0431f2d7ff2186576b681441
SxtTxch <kacper.oleksy2@wp.pl>
2024-11-11 21:02:58 +0100

    scroll animation, better background

--- index.js
+++ index.js
@@ -72,4 +72,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Load default content (e.g., "O Nas") on page load
     loadContent("onas");
+
+    // Scroll animacje
+    const scrollElements = document.querySelectorAll('.scroll-animate');
+
+    const observerOptions = {
+        threshold: 0.1, // Trigger when 10% of the element is in the viewport
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-up');
+                observer.unobserve(entry.target); // Stop observing once animation is triggered
+            }
+        });
+    }, observerOptions);
+
+    scrollElements.forEach(element => observer.observe(element));
 });
